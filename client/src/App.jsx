08bacb48be77e4b8b681862d9798646361932d68
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-05-26 21:04:20 +0530

    Setup front end in react vite and bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <h1 className="mt-5">React app</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
